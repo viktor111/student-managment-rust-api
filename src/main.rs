ea4297282edd55447e74eef38af7358e9d116efd
viktor111 <swifrorlilko@gmail.com>
2022-06-05 00:14:17 +0300

    add

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use actix_web::{App, HttpServer};
+
+mod students;
+mod response;
+mod constants;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(students::get)
+            .service(students::create)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
---
+++ src/students.rs
@@ -0,0 +1,59 @@
+use uuid::Uuid;
+use actix_web::web::{Json, Path};
+use actix_web::{post, get, HttpResponse};
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+
+
+use crate::response::Response;
+use crate::constants::APPLICATION_JSON;
+
+pub type Students = Response<Student>;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Student {
+	pub id: String,
+	pub created_at:  DateTime<Utc>,
+	pub name: String,
+}
+
+impl Student{
+	pub fn new(name: String) -> Self {
+		Self {
+			id: Uuid::new_v4().to_string(),
+			created_at: Utc::now(),
+			name
+		}
+	}
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct StudentRequest{
+	pub name: Option<String>
+}
+
+impl StudentRequest{
+	pub fn to_student(&self) -> Option<Student>{
+		match &self.name{
+			Some(name) => Some(Student::new(name.to_string())),
+			None => None
+		}
+	}
+}
+
+#[post("/student")]
+pub async fn create(student_request: Json<StudentRequest>) -> HttpResponse{
+	return HttpResponse::Created()
+		.content_type(APPLICATION_JSON)
+		.json(student_request.to_student())
+}
+
+#[get("/student")]
+pub async fn get() -> HttpResponse {
+    let name = String::from("Ivan Minkov");
+	let student = Student::new(name);
+
+	return HttpResponse::Ok()
+		.content_type(APPLICATION_JSON)
+		.json(student)
+}
