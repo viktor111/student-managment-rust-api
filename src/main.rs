557af255ab080cecefd5cc6364bdf0f751b815cd
viktor111 <swifrorlilko@gmail.com>
2022-06-05 11:15:47 +0300

    Add Dockerfile to build image

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ async fn main() -> io::Result<()> {
             .service(students::create)
             .service(students::all)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
